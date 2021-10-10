49a5e88c086c8d089eb58fb71e4c407dc8e4a20e
Albert Calasanz <acalasanzs@gmail.com>
2021-10-10 23:34:32 +0200

    d

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup
-setup(name='assgnopts',
-      version='1.0',
-      description='A plugin to ask the user',
-      url='https://github.com/acalasanzs/assgnopts',
-      author='Albert Calasanz',
-      author_email='acsestudios02@gmail.com',
-      license='MIT',
-      packages=['assgn',"opts"],
-      zip_safe=False)
 from distutils.core import setup
 setup(
   name = 'assgnopts',         # How you named your package folder (MyLib)
@@ -16,10 +6,10 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "It's a console input improvement, and and options in class digger",   # Give a short description about your library
   author = 'Albert Calasanz',                   # Type in your name
-  author_email = 'acsstudiosdev@gmail.com',      # Type in your E-Mail
+  author_email = 'acsestudios02@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/acsstudios/assgnopts',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/acsstudios/assgnopts/archive/refs/tags/Assgn1.0.tar.gz',    # I explain this later on
-  keywords = ['assgn', 'input', 'optionsinclass'],   # Keywords that define your package best
+  keywords = ['assgn', 'opt'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -31,5 +21,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
