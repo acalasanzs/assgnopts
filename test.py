a36eaae6b799f7727d55a49654ebc1b252198a8b
acsstudios <acsstudiosdev@gmail.com>
2021-10-14 20:11:36 +0200

    D

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from assgnopts import *
-import pandas
+import urllib.request
+import pandas as pd
+import numpy as np
 
-diccionari = {
-    0: ("Perse","Kilos"),
-    1: ("Pomes","unitats")
-}
-Les_meves_coses = Assgn(diccionari,conj="com a")
-Les_meves_coses.input()
-Les_meves_coses.dispValues()
-Les_meves_coses.save()
+urllib.request.urlretrieve(
+    'https://raw.githubusercontent.com/knadh/xmlutils.py/master/samples/fruits.csv', 
+    'fruits.csv')
+
+data = pd.read_csv ("fruits.csv")   
+df = pd.DataFrame(data, columns= ['Apple'])
+fruits = ['Apple']+df['Apple'].tolist()
+MyStuff = Assgn(Ar2Dict(fruits,"kilos"),conj="as")
+MyStuff.input()
+MyStuff.save()
+MyStuff.dispValues()
