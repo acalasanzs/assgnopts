d711b38ec6fc617bf94507d9d39f6801444a8929
Albert Calasanz <acalasanzs@gmail.com>
2021-10-17 19:36:13 +0200

    d

--- tobin.py
+++ tobin.py
@@ -7,4 +7,7 @@ with open('bin.csv', mode='w',newline='') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     employee_writer.writerow(['Char', 'Bin 2^6'])
     for x in range(36):
-        employee_writer.writerow([alfanum[x],bin(x)[2:]])
+        result = bin(x)[2:]
+        while len(result) < 6:
+            result = '0'+result
+        employee_writer.writerow([alfanum[x],result])
