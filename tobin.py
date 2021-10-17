305d65d59b4ca8cfa7eba1e33090846ad938b3fb
Albert Calasanz <acalasanzs@gmail.com>
2021-10-17 19:32:59 +0200

    bin

--- tobin.py
+++ tobin.py
@@ -1,6 +1,10 @@
 import csv
+import string
+
+alfanum = [x for x in string.ascii_uppercase]+[x for x in range(10)]
 
 with open('bin.csv', mode='w',newline='') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer.writerow(['Char', 'Bin 2^6'])
     for x in range(36):
-        employee_writer.writerow([bin(x)[2:]])
+        employee_writer.writerow([alfanum[x],bin(x)[2:]])
